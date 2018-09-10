c38d3e6d93b9b86d897ef294e58a5e95287c76ec
bhavesh2408 <bhaveshs2408@gmail.com>
2018-09-10 17:04:23 +0530

    Product Category added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_100921) do
+ActiveRecord::Schema.define(version: 2018_09_10_104401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_09_10_100921) do
     t.string "size", array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
