1078ee842a459bfc2a6f2f66374a3169ea8af2d3
bhavesh2408 <bhaveshs2408@gmail.com>
2018-09-14 18:01:26 +0530

    Array fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
       :name => row["Product Name"].to_s.squish,
       :code => row["Product Code"].to_s.squish,
       :price => row["Price"].to_s.squish,
-      :size => [row["Size"].to_s.squish],
+      :size => row["Size"].to_s.squish.split(","),
       :category => row["Category"].to_s.squish,
       :quantity => row["Quantity"].to_s.squish
     }
