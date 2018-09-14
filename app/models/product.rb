d272d441be8d1657c0ab5ebceb7a6ff7f1fe81da
bhavesh2408 <bhaveshs2408@gmail.com>
2018-09-14 17:26:17 +0530

    Array Size Fixes in import

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,13 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
+
   def self.attributes_for_import(row)
     {
       :name => row["Product Name"].to_s.squish,
       :code => row["Product Code"].to_s.squish,
       :price => row["Price"].to_s.squish,
-      :size => row["Size"].to_s.squish,
+      :size => [row["Size"].to_s.squish],
       :category => row["Category"].to_s.squish,
       :quantity => row["Quantity"].to_s.squish
     }
